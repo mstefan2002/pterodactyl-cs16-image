092fc581d641e2ce105fa8c0c3becc2188164873
Stefan <88290275+mstefan2002@users.noreply.github.com>
2025-08-05 02:22:39 +0300

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /home/container
-sleep 1
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
-
-MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo -e ":/home/container$ ${MODIFIED_STARTUP}"
-
-eval ${MODIFIED_STARTUP}
